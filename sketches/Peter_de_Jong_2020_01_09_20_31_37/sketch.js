b11b9fb73bc76d47104807329d62801525ac4b71
lynntf <32374143+lynntf@users.noreply.github.com>
2020-01-13 11:21:31 -0600

    Update sketch.js

--- sketches/Peter_de_Jong_2020_01_09_20_31_37/sketch.js
+++ sketches/Peter_de_Jong_2020_01_09_20_31_37/sketch.js
@@ -15,6 +15,8 @@ exposures = 0;
 min_dim = 500;
 shift_x = 0;
 shift_y = 0;
+m = 0;
+q = 0;
 
 
 let travelers = [];
@@ -27,48 +29,49 @@ function setup() {
   b = -2.53;
   c = 1.61;
   d = -0.33;
-  for(i = 0; i<maxnum; i++){
+  for (i = 0; i < maxnum; i++) {
     travelers[i] = new traveler(i);
     num++;
   }
-/*  min_dim = min(displayWidth, displayHeight);
-  if (displayWidth > displayHeight) {
-    shift_x = (displayWidth - displayHeight)/2;
-    //xmin = xmin*(displayWidth/displayHeight);
-    //xmax = xmax*(displayWidth/displayHeight);
-  } else {
-    shift_y = (- displayWidth + displayHeight)/2;
-    //ymin = ymin*(displayHeight/displayWidth);
-    //ymax = ymax*(displayHeight/displayWidth);
-  }*/
 }
 
 function draw() {
   if (exposures < maxexposures) {
-    for(i = 0; i < maxnum; i++) {
+    for (i = 0; i < maxnum; i++) {
       travelers[i].draw();
     }
     exposures += num;
   }
+  q++;
+  if (q > 15) {
+    q = 0;
+    m = 0;
+  }
 }
 
-function touchStarted(){
-  background(255, 255, 255);
-  a = random(-PI, PI);
-  b = random(-PI, PI);
-  c = random(-PI, PI);
-  d = random(-PI, PI);
-  exposures = 0;
-  for(i = 0; i<maxnum; i++){
-    travelers[i].rebirth();
+function touchStarted() {
+  m++;
+  q = 0;
+  if (m > 1) {
+    background(255, 255, 255);
+    a = random(-PI, PI);
+    b = random(-PI, PI);
+    c = random(-PI, PI);
+    d = random(-PI, PI);
+    exposures = 0;
+    for (i = 0; i < maxnum; i++) {
+      travelers[i].rebirth();
+    }
+    print("Reset image----------")
+    print("a:" + a + "")
+    print("b:" + b + "")
+    print("c:" + c + "")
+    print("d:" + d + "\n")
+    stroke(0, 0, 0, 5);
+    m = 0;
   }
-  print("Reset image----------")
-  print("a:" + a + "")
-  print("b:" + b + "")
-  print("c:" + c + "")
-  print("d:" + d + "\n")
-  stroke(0, 0, 0, 5);
 }
+
 
 class traveler {
   constructor(i) {
@@ -83,8 +86,8 @@ class traveler {
 
   draw() {
     stroke(0, 0, 0, 5);
-    this.xn = (sin(a * this.y ) - cos(b * this.x ));
-    this.yn = (sin(c * this.x ) - cos(d * this.y ));
+    this.xn = (sin(a * this.y) - cos(b * this.x));
+    this.yn = (sin(c * this.x) - cos(d * this.y));
     this.x = this.xn;
     this.y = this.yn;
     if (i > startup) {
@@ -94,11 +97,11 @@ class traveler {
       point(this.xp, this.yp);
     }
     this.age++;
-    if(this.age > max_iters) {
+    if (this.age > max_iters) {
       this.rebirth();
     }
   }
-  
+
   rebirth() {
     this.x = random(xmin, xmax);
     this.y = random(ymin, ymax);
